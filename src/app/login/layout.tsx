1776633370243972f71fff5f3b07a6546e04e31f
fred <fredyrs.wdev@gmail.com>
2024-06-18 16:10:05 -0500

    fix: shrink image for mobile in signup and login components, now it hides

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -4,10 +4,10 @@ import logo from "@/app/assets/logo.png";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section className="flex h-screen w-full items-center justify-center bg-gray-100 dark:bg-gray-950">
-      <div className="grid w-full max-w-5xl grid-cols-1 gap-6 rounded-lg bg-white p-6 shadow-lg dark:bg-gray-900 sm:grid-cols-2">
+      <div className="grid w-full max-w-5xl grid-cols-1 gap-6 rounded-lg bg-white p-6 m-8 shadow-lg dark:bg-gray-900 md:grid-cols-2">
         <div className="space-y-4">{children}</div>
-        <div className="flex flex-col items-center justify-center space-y-4 rounded-lg bg-gradient-to-br from-gray-500 to-black p-8 text-white">
-          <Image src={logo} alt="Wisdomia Logo" width={50} height={50}  />  
+        <div className="hidden items-center justify-center space-y-4 rounded-lg bg-gradient-to-br from-gray-500 to-black p-8 text-white md:flex flex-col">
+          <Image src={logo} alt="Wisdomia Logo" width={50} height={50} />
           <h2 className="text-3xl font-bold">Welcome to Wisdomia!</h2>
           <p className="text-lg text-center">
             Inicia sesión para acceder a todas las funcionalidades de Wisdomia.
--- src/app/signup/layout.tsx
+++ src/app/signup/layout.tsx
@@ -4,8 +4,8 @@ import logo from "@/app/assets/logo.png";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section className="flex h-screen w-full items-center justify-center bg-gray-100 dark:bg-gray-950">
-      <div className="grid w-full max-w-5xl grid-cols-1 gap-6 rounded-lg bg-white p-6 shadow-lg dark:bg-gray-900 sm:grid-cols-2">
-        <div className="flex flex-col items-center justify-center space-y-4 rounded-lg bg-gradient-to-br from-gray-500 to-black p-8 text-white">
+      <div className="grid w-full max-w-5xl grid-cols-1 gap-6 rounded-lg bg-white m-8 p-6 shadow-lg dark:bg-gray-900 md:grid-cols-2">
+        <div className="hidden items-center justify-center space-y-4 rounded-lg bg-gradient-to-br from-gray-500 to-black p-8 text-white md:flex flex-col">
           <Image src={logo} alt="Wisdomia Logo" width={50} height={50}  />
           <h2 className="text-3xl font-bold">Welcome to Wisdomia!</h2>
           <p className="text-lg text-center">
